3011603850b0b7187f426cbe95bacb6931f710a1
Simone Salerno <doppiaesse@proton.me>
2026-07-12 22:17:35 +0200

    relay 0.9.0

--- Casks/relay.rb
+++ Casks/relay.rb
@@ -1,6 +1,6 @@
 cask "relay" do
-  version "0.8.2"
-  sha256 "0f8a8b66c0ffc83a5a33b88a52ce10e8d53d4bdc8671bc5cf5235d6af380b30a"
+  version "0.9.0"
+  sha256 "5b3cb816993a5d5c0eb7b177bde47da9512994996f84b30a3ae65d75d3647548"
 
   url "https://github.com/essedev/relay/releases/download/v#{version}/Relay-#{version}.dmg"
   name "Relay"
